a3e41b8bce7142c40e658ad020b6c5277d6f3935
gladyn <glds.nvrr@gmail.com>
2019-05-22 08:28:42 -0700

    Updates

---
+++ precache-manifest.51721b48ba6509a38a079a9188b2840d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2faf7398fa8879865bf809f99726915b",
+    "url": "/Clicky-Game-/index.html"
+  },
+  {
+    "revision": "8af7084f93faec87fd4e",
+    "url": "/Clicky-Game-/static/css/main.2512994b.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/Clicky-Game-/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "8af7084f93faec87fd4e",
+    "url": "/Clicky-Game-/static/js/main.0f989202.chunk.js"
+  },
+  {
+    "revision": "8d0b7416d9b563643a85",
+    "url": "/Clicky-Game-/static/js/runtime~main.4bbcbc5a.js"
+  }
+]);
